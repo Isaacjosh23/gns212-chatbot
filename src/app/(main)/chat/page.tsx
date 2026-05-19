c86ca93c9ab799c7e586e5152f82d86216d163f4
Isaacjosh23 <isaacjoshua969@gmail.com>
2026-05-19 09:30:01 +0100

    Fixed chat mobile view

--- src/app/(main)/chat/page.tsx
+++ src/app/(main)/chat/page.tsx
@@ -7,12 +7,12 @@ import { ChatSidebar } from "@/components/chat/ChatSidebar";
 
 export default function ChatPage() {
   return (
-    <div className="h-screen overflow-hidden flex w-full h-screen bg-(--bg)">
+    <div className="h-screen overflow-hidden flex w-full bg-(--bg)">
       <div className="hidden bg-[var(--bg-surface)] md:flex md:flex-col">
         <ChatSidebar />
       </div>
 
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 min-h-0">
         <ChatHeader />
 
         <ChatWindow />
